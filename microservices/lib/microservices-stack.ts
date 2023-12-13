d52ea1cc8b05ffc4b38621cd161da655bf3ae938
iamchapita <alejandrom646@gmail.com>
2023-12-13 11:44:54 -0600

    Remoción de código comentado

--- microservices/lib/microservices-stack.ts
+++ microservices/lib/microservices-stack.ts
@@ -16,51 +16,5 @@ export class MicroservicesStack extends cdk.Stack {
 		const apiGateway = new SwnApiGateway(this, "ApiGateway", {
 			productMicroservice: microservices.productMicroservice,
 		});
-		/* const productTable = new Table(this, "product", {
-			partitionKey: {
-				name: "id",
-				type: AttributeType.STRING,
-			},
-			tableName: "product",
-			removalPolicy: cdk.RemovalPolicy.DESTROY,
-			billingMode: BillingMode.PAY_PER_REQUEST,
-		}); */
-
-		/* const nodejsFunctionProps: NodejsFunctionProps = {
-			bundling: {
-				externalModules: ["aws-sdk"],
-			},
-			environment: {
-				PRIMARY_KEY: "id",
-				DYNAMODB_TABLE_NAME: database.productTable.tableName,
-			},
-			runtime: Runtime.NODEJS_20_X,
-		};
-
-		const productFunction = new NodejsFunction(
-			this,
-			"productLambdaFunction",
-			{
-				entry: join(__dirname, "../src/product/index.js"),
-				...nodejsFunctionProps,
-			}
-		); 
-		
-		database.productTable.grantReadWriteData(productFunction);*/
-
-		/* const apiGateway = new LambdaRestApi(this, "productAPI", {
-			restApiName: "Product Service",
-			handler: microservices.productMicroservice,
-			proxy: false,
-		});
-
-		const product = apiGateway.root.addResource("product");
-		product.addMethod("GET");
-		product.addMethod("POST");
-
-		const singleProduct = product.addResource("{id}");
-		singleProduct.addMethod("GET");
-		singleProduct.addMethod("PUT");
-		singleProduct.addMethod("DELETE"); */
 	}
 }
